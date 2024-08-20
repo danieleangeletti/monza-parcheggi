0c5f9713f8d7db81de8e1e1c7357a027193bab0f
danieleangeletti <146859541+danieleangeletti@users.noreply.github.com>
2024-08-20 09:31:54 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('page-title', 'Home')
+
+@section('main-content')
+    <main>
+        <div class="container">
+            MAIN
+        </div>
+    </main>
+@endsection
